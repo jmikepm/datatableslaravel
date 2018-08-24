d4836e2de72d54c660c1d15b6521e6a708317391
Mike Pena <jmiguelpm14@gmail.com>
2018-08-24 15:47:53 -0500

    subiendo proyecto de datatables a git

---
+++ resources/views/index.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DataTable</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+        
+
+        <!-- Styles -->
+       
+    </head>
+    <body>
+        <div class="container">
+            <table id="users" class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+
+            </table>  
+        
+        </div>
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+        <script>
+            $(document).ready(function() {
+                $('#users').DataTable({
+                    "serverSide": true,
+                    "ajax":"{{ url('api/users') }}",
+                    "columns":[
+                        {data: 'id'},
+                        {data: 'name'},
+                        {data: 'email'},
+                    ]
+                });
+            } );
+        </script>
+    </body>
+</html>
